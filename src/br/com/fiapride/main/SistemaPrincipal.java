c454df72e983f1b6a6c64eb0c527efb47d297eb1
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-19 10:02:28 -0300

    Microdesafio - Classe BilheteUnico

---
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -0,0 +1,28 @@
+package br.com.fiapride.main;
+
+import br.com.fiapride.model.BilheteUnico;
+import java.time.LocalDate;
+
+public class SistemaPrincipal {
+	
+   public static void main(String[] args) {
+	   
+       // Instância 1
+       BilheteUnico bilhete1 = new BilheteUnico();
+       bilhete1.tipo = "estudante";
+       bilhete1.saldo = 20.00;
+       bilhete1.dataValidade = LocalDate.of(2026, 12, 31);
+       
+       // Instância 2
+       BilheteUnico bilhete2 = new BilheteUnico();
+       bilhete2.tipo = "comum";
+       bilhete2.saldo = 5.00;
+       bilhete2.dataValidade = LocalDate.of(2025, 5, 10);
+       
+       System.out.println("Bilhete 1 tipo: " + bilhete1.tipo
+    		   + "e saldo: " + bilhete1.saldo);
+       System.out.println("Bilhete 2 tipo: " + bilhete2.tipo
+       		+ "e saldo: " + bilhete2.saldo);
+       
+   }
+}
---
+++ src/br/com/fiapride/model/BilheteUnico.java
@@ -0,0 +1,13 @@
+package br.com.fiapride.model;
+
+import java.time.LocalDate;
+
+public class BilheteUnico {
+	
+
+    // características 
+    public String tipo;
+    public double saldo;
+    public LocalDate dataValidade;
+    
+}
